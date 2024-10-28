0b60c4d23fada28cb7174bea1cb157e173ce6962
yuyanget <845262968@qq.com>
2024-10-28 18:19:45 +0800

    fix 修改方法make_section

--- test/main.py
+++ test/main.py
@@ -1,18 +1,22 @@
-
-
 from xy_work.Settings.Section.Project import Project as xyProject
+
+
 class Project(xyProject):
     def get_name(self) -> str | None:
         return "xy_web_project"
 
+
 from xy_work.Settings.Section.Runner import Runner as xyWorkRunner
+
 
 class Runner(xyWorkRunner):
     def get_name(self) -> str | None:
         return "xy_web_runner"
 
+
 from pathlib import Path
 from xy_web_settings.Settings import Settings as xySettings
+
 
 class Settings(xySettings):
     project: Project | None
@@ -20,8 +24,8 @@ class Settings(xySettings):
 
     def reload(self, settings_cfg_path: Path):
         super().reload(settings_cfg_path)
-        self.project = self.__make_section(Project)
-        self.runner = self.__make_section(Runner)
+        self.project = self.make_section(Project)
+        self.runner = self.make_section(Runner)
 
 
 if __name__ == "__main__":
